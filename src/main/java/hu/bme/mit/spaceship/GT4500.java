17f6f4306781f1a65acb9aacaf99efb4130dbabb
akapush <laborbalepos@gmail.com>
2022-04-08 13:04:27 +0200

    quick fix of all firing mode in GT4500

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,8 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
-
+        //TODO rendesen megcsinálni
+        firingSuccess = true; 
         break;
     }
 
