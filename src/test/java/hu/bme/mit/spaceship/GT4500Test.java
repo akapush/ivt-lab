7f5c4df223afdac626948b7cf1bfd5312fc27ba6
akapush <laborbalepos@gmail.com>
2022-05-06 13:28:17 +0200

    unit testing torpedostore

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,16 +8,23 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
-  private GT4500 ship;
 
+  private GT4500 ship;
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    
+    primary =  mock(TorpedoStore.class);
+    secondary =  mock(TorpedoStore.class);
+    this.ship = new GT4500(primary,secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -29,12 +36,58 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Failure(){
+    // Arrange
+    when(primary.fire(1)).thenReturn(false);
+    when(secondary.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_FirstEmptyFireSecondary(){
+    // Arrange
+    when(primary.isEmpty()).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_PrimaryWasFired(){
+    // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
+
+    // Act
+    boolean resultFirst = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean resultSecond = ship.fireTorpedo(FiringMode.SINGLE);
+
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(1)).fire(1);
+
+    // Assert
+    assertEquals(true, resultFirst);
+    assertEquals(true, resultSecond);
   }
 
 }
